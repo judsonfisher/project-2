0a285ab67d626a97f1de5ab777cfbe2a1081218a
Your Namejudsonfisher <judsonefisher@gmail.com>
2017-11-18 10:50:54 -0600

    server changes

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var exphbs = require("express-handlebars");
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -17,18 +16,6 @@ app.use(express.static("public"));
 require("./routes/html-routes.js")(app);
 require("./routes/api-routes.js")(app);
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
-
-app.get("/", function(req, res) {
-    db.all(function(data) {
-      var hbsObject = {
-        db: data
-      };
-      console.log(hbsObject);
-      res.render("index", hbsObject);
-    });
-  });
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
