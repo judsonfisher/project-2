d9ee047c45271ac05ab83425f12aec0028e1b0c0
judsonfisher <judsonefisher@gmail.com>
2017-11-30 16:42:26 -0600

    new db info

--- app/routes/article-api-routes.js
+++ app/routes/article-api-routes.js
@@ -1,5 +1,4 @@
 var db = require("../models");
-
 // =============================================================
 
 module.exports = function(app) {
@@ -7,20 +6,19 @@ module.exports = function(app) {
   //GET route for getting all of the todos
   app.get("/api/news", function(req, res) {
     // findAll returns all entries for a table when used with no options
-    console.log("working")
-    db.Articles.findAll({
-      include: [] // data goes here
-    }).then(function(dbNews) {
+    console.log("Working");
+    db.articles.findAll({}).then(function(dbNews) {
       res.json(dbNews);
     });
   });
 
   app.post("/api/news", function(req, res) {
-    db.Articles.create({
+    db.articles.create({
       published: req.body.published,
       title: req.body.title,
-      thread_title: req.body.threadTitle,
-      main_img_url: req.body.imgURL
+      siteUrl: req.body.siteUrl
+      threadTitle: req.body.threadTitle,
+      mainImgUrl: req.body.mainImgUrl
     }).then(function(dbNews) {
       res.json(dbNews);
     });
--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(express.static("app/public"));
 require("./app/routes/html-routes.js")(app);
 require("./app/routes/api-routes.js")(app);
 
-db.sequelize.sync({ force: true }).then(function() {
+db.sequelize.sync({force: true}).then(function() {
   app.listen(PORT, function() {
     console.log("App listening on PORT " + PORT);
   });
@@ -28,3 +28,29 @@ db.sequelize.sync({ force: true }).then(function() {
 // - Create front end JS files that execute requests once buttons are clicked
 // - Link HTML routes to those commands
 // - Hardcode DB to app and talk to TAs when time to deploy
+
+// CREATE TABLE `articles` (
+//   id int(11) NOT NULL AUTO_INCREMENT,
+//   published VARCHAR(255),
+//   title VARCHAR(1000),
+//   site_url VARCHAR(1000),
+//   thread_title VARCHAR(1000),
+//   main_img_url VARCHAR(1000),
+//   createdAt datetime NOT NULL,
+//   updatedAt datetime NOT NULL,
+//   PRIMARY KEY (`id`)
+// )
+
+// CREATE TABLE `articles` (
+//   published VARCHAR(255),
+//   title VARCHAR(1000),
+//   site_url VARCHAR(1000),
+//   thread_title VARCHAR(1000),
+//   main_img_url VARCHAR(1000),
+// )
+
+// db.sequelize.sync({force: true}).then(function() {
+//   app.listen(PORT, function() {
+//     console.log("App listening on PORT " + PORT);
+//   });
+// });
